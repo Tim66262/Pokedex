9958f57d87b1eff05a087205f6d91a1270cf3ae1
Tim66262 <tim.damato@swisscom.com>
2018-05-29 16:34:23 +0200

    asdf

--- Pokedex/app/src/main/java/com/example/brippp/pokedex/MainActivity.java
+++ Pokedex/app/src/main/java/com/example/brippp/pokedex/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.brippp.pokedex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /**
@@ -17,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void onClickEi(View view) {
+        counter += 1;
+        if(counter > 5){
 
+        }
+        if(counter > 10){
+
+        }
+        if(counter > 15){
+
+        }
     }
 }
